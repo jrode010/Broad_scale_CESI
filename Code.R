58f7392854b584147d10ea2349239723c514b1aa
jrode010 <jrode010@fiu.edu>
2024-10-10 15:03:58 -0400

    Initial commit

---
+++ Code.R
@@ -0,0 +1,8 @@
+###Working with CESI broad-scale data for residency
+##Goal: Look at drivers for seatrout to leave the system (Rankin)
+##Hypotheses: Seatrout will vacate the system when a certain temperature theshold is reached - Temperature is going to be most important
+##Methodology: Get presence/absence data and go from there. Maybe identify a threshold by modelling data close to departure. Or model presence/absence
+
+library(librarian)
+
+shelf(tidyverse, )
